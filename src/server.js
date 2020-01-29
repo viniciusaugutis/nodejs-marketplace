a757b38d891a5ffda7b993778541ad910ad80f7d
Vinícius Augutis <vinicius_augutis@hotmail.com>
2020-01-29 20:12:59 -0300

    Configurando mongoose no projeto

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const databaseConfig = require('../config/database')
 
 class App {
   constructor () {
     this.express = express()
     this.isDev = process.env.NODE_ENV !== 'production'
+    this.database()
     this.middlewares()
     this.routes()
   }
@@ -14,6 +17,14 @@ class App {
 
   routes () {
     this.express.use(require('./routes'))
+  }
+
+  database () {
+    // mongoose.connect('mongodb://usuario:senha@localhost:27017/nomedatabase') - quando roda local
+    mongoose.connect(databaseConfig.uri, {
+      useCreateIndex: true,
+      useNewUrlParser: true
+    })
   }
 }
 
