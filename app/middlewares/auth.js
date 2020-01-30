d29e5a1511bdda7128e9b8d4d7ff41de676bbac5
Vinícius Augutis <vinicius_augutis@hotmail.com>
2020-01-29 21:14:20 -0300

    Criando middleware para verificação de token Bearer

---
+++ app/middlewares/auth.js
@@ -0,0 +1,21 @@
+const authConfig = require('../../config/auth')
+const jwt = require('jsonwebtoken')
+const { promisify } = require('util')
+
+module.exports = async (req, res, next) => {
+  const authHeader = req.headers.Authorization
+
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token not provided' })
+  }
+
+  const [, token] = authHeader.split(' ')
+
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret)
+    req.userId = decoded.id // decoded tem todos dados que foram salvos junto do token jwt
+    return next()
+  } catch (err) {
+    return res.status(401).json({ error: 'Token is invalid' })
+  }
+}
--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,11 @@ const routes = express.Router()
 
 const UserController = require('../app/controllers/UserController')
 const SessionController = require('../app/controllers/SessionController')
+const authMiddleware = require('../app/middlewares/auth')
 
 routes.post('/users', UserController.store)
+
+routes.use(authMiddleware)
 routes.post('/sessions', SessionController.store)
 
 module.exports = routes
