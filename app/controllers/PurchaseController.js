db03f86bdb806568e3d201086a1b724a8b4d53b3
Vinícius Augutis <vinicius_augutis@hotmail.com>
2020-01-30 21:07:51 -0300

    Configurando fila do redis para envio de email

--- app/controllers/PurchaseController.js
+++ app/controllers/PurchaseController.js
@@ -1,20 +1,19 @@
 const Ad = require('../models/Ad')
 const User = require('../models/User')
-const Mail = require('../services/Mail')
-
+const Queue = require('../services/Queue')
+const PurchaseMail = require('../jobs/PurchaseMail')
 class PurchaseController {
   async store (req, res) {
     const { ad, content } = req.body
     const purchaseAd = await Ad.findById(ad).populate('author')
     const user = await User.findById(req.userId)
 
-    await Mail.sendMail({
-      from: '"Vinicius Augutis" <vinicius_augutis@hotmail.com>',
-      to: purchaseAd.author.email,
-      subject: `Solicitação de compra: ${purchaseAd.title}`,
-      template: 'purchase',
-      context: { user, content, ad: purchaseAd }
-    })
+    Queue.create(PurchaseMail.key, {
+      ad: purchaseAd,
+      user,
+      content
+    }).save()
+
     return res.send()
   }
 }
---
+++ app/jobs/PurchaseMail.js
@@ -0,0 +1,25 @@
+const Mail = require('../services/Mail')
+
+class PurchaseMail {
+  get key () {
+    return 'PurchaseMail'
+  }
+
+  async handle (job, done) {
+    // job é o job em si e done é chamada quando terminar de executar o job
+
+    const { ad, user, content } = job.data
+
+    await Mail.sendMail({
+      from: '"Vinicius Augutis" <vinicius_augutis@hotmail.com>',
+      to: ad.author.email,
+      subject: `Solicitação de compra: ${ad.title}`,
+      template: 'purchase',
+      context: { user, content, ad }
+    })
+
+    return done()
+  }
+}
+
+module.exports = new PurchaseMail()
